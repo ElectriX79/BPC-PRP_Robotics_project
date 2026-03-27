6454f9933e169a0ce6f6636256a8ce1edba2e0ee
Samuel Chovan <xchovas00@butbr.cz>
2026-03-27 17:23:15 +0100

    scripts remove

--- all.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd /root/ros_w/build/prp_project
-./line_follower_node
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-gedit /root/ros_w/src/bpc-prp-cpp-project-template/include/nodes/line_follower_node.hpp
-
